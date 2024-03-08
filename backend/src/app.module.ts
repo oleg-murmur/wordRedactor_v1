0d19341cd68808b09351916c81e0d2c3eb05f499
olegmurmur <oleg5052002505@gmail.com>
2024-03-08 15:25:38 +0600

    fix bug api not working on port 3000

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,26 +14,40 @@ import { UserModule } from './user/user.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        // type: 'postgres',
-        // host: 'db',
-        // port: 5432,
-        // username: 'postgres',
-        // password: 'postgres',
-        // database: 'postgres',
+        type: 'postgres',
+        host: 'db',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'postgres',
         // entities: [],
-        // // entities: [join(process.cwd(),'dist/**/*.entity.js')]
+        entities: [join(process.cwd(),'dist/**/*.entity.js')],
+        synchronize: true,
+        autoLoadEntities: true,
+        
+        
+        /* with docker env */
+        // type: 'postgres',
+        // host: configService.get('DB_HOST_DOCKER'),
+        // port: configService.get('DB_PORT_DOCKER'),
+        // username: configService.get('DB_USER_DOCKER'),
+        // password: configService.get('DB_PASS_DOCKER'),
+        // database: configService.get('DB_NAME_DOCKER'),
+        // entities: [join(process.cwd(),'dist/**/*.entity.js')],
         // synchronize: true,
         // autoLoadEntities: true,
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASS'),
-        database: configService.get('DB_NAME'),
-        entities: [join(process.cwd(),'dist/**/*.entity.js')],
 
+        //without docker
+        // type: 'postgres',
+        // host: configService.get('DB_HOST'),
+        // port: configService.get('DB_PORT'),
+        // username: configService.get('DB_USER'),
+        // password: configService.get('DB_PASS'),
+        // database: configService.get('DB_NAME'),
+        // entities: [join(process.cwd(),'dist/**/*.entity.js')],
+        // synchronize: true,
         // do not use sync: true in real project
-        synchronize: true,
+
 
       })
     }),
